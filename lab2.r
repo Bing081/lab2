23db4d540c016aaf9ce6c0813f6361108f89e3c8
Ken Sund <47672499+KenCanary@users.noreply.github.com>
2020-10-13 23:41:14 +0200

    Update lab2.r

--- lab2.r
+++ lab2.r
@@ -5,33 +5,32 @@ data=shootings_1[sample(4895, 100, replace=TRUE),]
 
 data=droplevels(data)
 
-# Summary & Visualisation
+# --------------------------------- Summary & Visualisation --------------------------------- #
 summary(data)
 
-#Create histograms (?) of age, fleeing, signs of mental illness
-
-# --------------- Probability plot for age ------------- #
+# Check Normality assumption for 'age'
 qqnorm(data$age)
 qqline(data$age)
-# ------------------------------------------------------ #
 
-# --------------- Histogram for "Age" ------------------ #
+# Histogram for age
 n = 0:length(data$age)
 y = dnorm(n, mean=mean(data$age), sd=sd(data$age))
 
 plot(n, y, type ='h')
-# ------------------------------------------------------ #
 
-# ------ Histogram for "Signs of mental illness" ------- #
+# Histogram for 'signs of mental illness'
 n = 0:length(data$signs_of_mental_illness)
 x = length(data$signs_of_mental_illness[data$signs_of_mental_illness==TRUE])
 p = x/length(data$signs_of_mental_illness)
 
 y = dbinom(n, size=x, prob = p)
 plot(n, y, type = 'h')
-# ------------------------------------------------------ #
 
-# Question 1
+# Histogram for 'armed'
+
+# --------------------------------------- Question 1 ---------------------------------------- #
+
+# Initialize 
 xbar = mean(data = data$flee)
 s = sd(data$age)
 n = length(data$age)
@@ -40,16 +39,19 @@ alpha=0.05
 z = qnorm(alpha, lower.tail = FALSE)
 l = xbar-z*s/sqrt(n)
 
+# --------------------------------------- Question 2 ---------------------------------------- #
 
-# Question 2
 x = length(data$armed[data$armed=="unarmed"])
 phat = x/length(data$armed)
 n = length(data$armed)
 alpha=0.05
 binom.test(x, n, phat, alternative = "two.sided", conf.level = 1-alpha)
 
-# Question 3
+# --------------------------------------- Question 3 ---------------------------------------- #
 
-# Question 4
 
-# Question 5
+# --------------------------------------- Question 4 ---------------------------------------- #
+
+
+# --------------------------------------- Question 5 ---------------------------------------- #
+
